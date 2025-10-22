b9a9bebb3555cb05526c037be18ce7e1b3f92935
Clinton Yade <yadeclinton20@gmail.com>
2025-10-22 20:12:06 +0300

    updated the postcss. config file

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
-const config = {
+/** @type {import('postcss-load-config').Config} */
+module.exports = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
   },
-};
-
-export default config;
+}
